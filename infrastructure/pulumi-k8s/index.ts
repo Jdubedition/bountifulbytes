a7fa03e98952ca77e04dce34b17eb5908cfbfd61
Justin Wesley <justin.t.wesley@gmail.com>
2021-04-18 20:59:35 -0600

    7 - add pulumi k8s code for deploy and svc

---
+++ infrastructure/pulumi-k8s/index.ts
@@ -0,0 +1,31 @@
+import * as k8s from "@pulumi/kubernetes";
+import * as kx from "@pulumi/kubernetesx";
+
+const isLocal = true;
+
+const appName = "justink8s-app";
+const appLabels = { app: "justink8s-app" };
+const deployment = new k8s.apps.v1.Deployment(appName, {
+    spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 1,
+        template: {
+            metadata: { labels: appLabels },
+            spec: { containers: [{ name: appName, image: "jdubedition/justink8s-app:2021.04.18.1" }] }
+        }
+    }
+});
+
+const frontend = new k8s.core.v1.Service(appName, {
+    metadata: { labels: deployment.spec.template.metadata.labels },
+    spec: {
+        type: isLocal ? "NodePort" : "LoadBalancer",
+        ports: [{
+            port: 8080,
+            targetPort: 80,
+            protocol: "TCP",
+            nodePort: 32000
+        }],
+        selector: appLabels
+    }
+});
