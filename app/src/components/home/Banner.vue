9adba83c438a5fcda90d84cfcccb753c34ddec21
Justin Wesley <justin.t.wesley@gmail.com>
2021-12-18 11:08:40 -0700

    update banner slogan

--- app/src/components/home/Banner.vue
+++ app/src/components/home/Banner.vue
@@ -6,21 +6,12 @@
       height="200"
       width="100%"
     >
-      <v-row
-        class="fill-height pa-3"
-        align="center"
-      >
-        <v-col
-          cols="12"
-          md="7"
-          offset-md="5"
-        >
-          <h1 class="display-2 font-weight-light">
-            BountifulBytes
-          </h1>
+      <v-row class="fill-height pa-3" align="center">
+        <v-col cols="12" md="7" offset-md="5">
+          <h1 class="display-2 font-weight-light">BountifulBytes</h1>
 
           <div class="subheading text-uppercase pl-2 mb-4">
-            To Kube or not to Kube
+            Here, There, and Everywhere
           </div>
 
           <!-- <v-btn
@@ -36,7 +27,7 @@
 </template>
 
 <script>
-  export default {
-    name: 'HomeBanner',
-  }
+export default {
+  name: "HomeBanner",
+};
 </script>
